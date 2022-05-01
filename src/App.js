fb75e853da5d6322503fb199b38cb3f70e39e52b
hiratkarharshal <88575911+hiratkarharshal@users.noreply.github.com>
2022-05-01 21:18:06 +0530

    react grid assign 1 done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Post from './Post';
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Post/>
+    <Footer/>
+  </div>
+  );
+}
+
+export default App;
